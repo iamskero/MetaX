5e7927768e2e9ac05b91febb1975121f929e947a
iamskero <irockefeller.jd@gmail.com>
2021-06-02 09:24:40 +0200

    Initial commit

---
+++ MetaX/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MetaX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
