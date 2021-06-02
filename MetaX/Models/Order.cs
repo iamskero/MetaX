9326339e485d8023d8b095e0a79b01ed89068a86
iamskero <irockefeller.jd@gmail.com>
2021-06-02 10:20:43 +0200

    Id is always null in data so property was null, changed to setter with Guid and a backing field Added a TxProcessor class which will take care of all the buys and sells, for now with little functionality

--- MetaX/Models/Order.cs
+++ MetaX/Models/Order.cs
@@ -4,7 +4,21 @@ namespace MetaX.Models
 {
     public class Order
     {
-        public string Id { get; set; }
+        private string _id { get; set; }
+
+
+        /// <summary>
+        /// faking id since they were omitted in the data
+        /// no collisions assumed
+        /// </summary>
+        public string Id
+        {
+            get
+            {
+                return _id.ToString();
+            }
+            set { this._id = Guid.NewGuid().ToString(); }
+        }
 
         public DateTime Time { get; set; }
 
---
+++ MetaX/TxProcessor.cs
@@ -0,0 +1,63 @@
+﻿using MetaX.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MetaX
+{
+    class TxProcessor
+    {
+        private List<Exchange> exchangesData;
+        public TxProcessor(List<Exchange> ed)
+        {
+            exchangesData = ed;
+        }
+
+        public List<object> FindBestBuy(decimal amountBTC)
+        {
+            var ordersFromAllExchanges = GetOrdersFromAllExchangesNormalized(false);
+
+            #region orders sorted by price desc
+            Func<Order, decimal> sortByPrice = o => o.Price;
+            var ordersSortedByPriceDesc = GetSortedOrders(ordersFromAllExchanges, sortByPrice, false);
+            #endregion
+
+            return null;
+        }
+
+        /// <summary>
+        /// Get the orders from all the exchanges in a single list
+        /// Param: asks bool
+        /// </summary>
+        /// <returns></returns>
+        private IEnumerable<Order> GetOrdersFromAllExchangesNormalized(bool asks = true)
+        {
+            if (asks)
+                return exchangesData.Select(i => i.OrderBook.OrderAsks)
+                .SelectMany(i => i);
+            else
+                return exchangesData.Select(i => i.OrderBook.OrderBids)
+                .SelectMany(i => i);
+        }
+
+
+        #region private sorting methods
+        /// <summary>
+        /// Sort orders by ...
+        /// TODO?: Could make a class with sorters, or a function that accepts list of sorting delegates or...or...or...
+        /// </summary>
+        /// <param name="orders"></param>
+        /// <param name="sortingFunc"></param>
+        /// <param name="sortAsc"></param>
+        /// <returns></returns>
+        private IOrderedEnumerable<Order> GetSortedOrders(IEnumerable<Order> orders, Func<Order, decimal> sortingFunc, bool sortAsc)
+        {
+            if (sortAsc)
+            {
+                return orders.OrderBy(sortingFunc);
+            }
+            else return orders.OrderByDescending(sortingFunc);
+        }
+        #endregion
+    }
+}
